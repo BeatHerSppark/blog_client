963d41c27b112f0bbfdf685fdc7c3a58b8dd6153
BeatHerSppark <peter@halkyon.com>
2023-08-30 20:00:14 +0200

    post added from server

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,50 +1,63 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import Menu from "../components/Menu";
+import axios from "axios";
+import moment from "moment";
+import { AuthContext } from "../context/authContext";
 
 const Single = () => {
+  const [post, setPost] = useState({});
+
+  const location = useLocation();
+
+  const postId = location.pathname.split("/")[2];
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`http://localhost:5000/posts/${postId}`);
+        setPost(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+    console.log(postId);
+    console.log(currentUser?.username);
+    console.log(post.username);
+  }, [postId]);
+
   return (
     <div className="single">
       <div className="content">
-        <img
-          src="https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          alt=""
-        />
+        <img src={post?.img} alt="" />
         <div className="user">
-          <img
-            src="https://eesa.lbl.gov/wp-content/uploads/2014/11/Gilbert-Ben-Portrait-Oct-2016-1.jpg"
-            alt=""
-          />
+          {post.userImg ? (
+            <img src={post.userImg} alt="" />
+          ) : (
+            <img
+              src="https://eesa.lbl.gov/wp-content/uploads/2014/11/Gilbert-Ben-Portrait-Oct-2016-1.jpg"
+              alt=""
+            />
+          )}
           <div className="info">
-            <span>Gilbert</span>
-            <p>Posted 2 days ago</p>
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
           </div>
-          <div className="edit">
-            <Link to={`/write?edit=2`}>
-              <button className="button__edit">Edit</button>
-            </Link>
-            <button className="button__delete">Delete</button>
-          </div>
+          {currentUser?.username === post.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`}>
+                <button className="button__edit">Edit</button>
+              </Link>
+              <button className="button__delete">Delete</button>
+            </div>
+          )}
         </div>
-        <h1>Lorem ipsum dolor blablablalbalbalba</h1>
-        <p>
-          For instance, HubSpot blogs about various topics concerning marketing,
-          sales, and service because HubSpot sells products related to those
-          three subjects -- so, more than likely, the type of readers HubSpot's
-          blog attracts are going to be similar to HubSpot's core buyer persona.
-          <br />
-          <br />
-          Alternatively, a woman named Kiki started a personal travel blog,
-          called The Blonde Abroad, to document her travel experiences and
-          provide readers with helpful tips and travel recommendations. Her blog
-          doesn't serve a larger company, but it does help her create a personal
-          brand.
-          <br />
-          <br />
-          If a personal blog is successful enough, the writer can also make
-          money off of it via sponsorships or advertisements. Take a look at 5
-          Strategies to Monetize a Blog to learn more.
-        </p>
+        <h1>{post.title}</h1>
+        {post.desc}
       </div>
       <Menu />
     </div>
